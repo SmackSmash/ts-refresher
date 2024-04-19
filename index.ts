54bc04adea38811f6596a76fc826fb98d1a9c2a9
Dan Moffat <danmoffat@gmail.com>
2024-04-19 19:15:53 +0100

    Object types

--- index.ts
+++ index.ts
@@ -6,3 +6,15 @@ let isValid = true; // Boolean
 let userId: string | number;
 userId = 'gahjsd';
 userId = 893469;
+// Object types
+const user: {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  id: string | number;
+} = {
+  name: 'Dan',
+  age: 37,
+  isAdmin: true,
+  id: 'abc' //123
+};
